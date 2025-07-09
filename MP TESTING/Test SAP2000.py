88101d499618794f4e94af80773c02daa7aa3189
Mpascual7 <mnwpascual@gmail.com>
2025-07-09 12:53:35 -0700

    Update Test SAP2000.py

--- MP TESTING/Test SAP2000.py
+++ MP TESTING/Test SAP2000.py
@@ -12,17 +12,27 @@ SapModel = mySapObject.SapModel
 kip_ft_F = 4
 SapModel.InitializeNewModel(kip_ft_F)
 
-# Use template to create 3D frame
-# Format: New3DFrame(template_type, numX, XBay, numY, YBay, numStory, StoryHeight)
 # Template types:
 #   1 = SteelDeck, 2 = ConcreteSlab, 3 = BeamSlab (what you're using), etc.
 BeamSlab = 3
+# Use template to create 3D frame
+# Format: New3DFrame(template_type, numX, XBay, numY, YBay, numStory, StoryHeight)
 ret = SapModel.File.New3DFrame(BeamSlab, 3, 12, 3, 28, 2, 36)
 
 # Optionally save and close
 ModelPath = os.path.join(os.getcwd(), '3DFrame.sdb')
+
+#Load case
+
+#ADD LOAD CASE HERE
+
+#Save and Run Analysis
 SapModel.File.Save(ModelPath)
 SapModel.Analyze.RunAnalysis()
+
+#Extract Results
+
+#ADD ^^^
 
 # Exit SAP2000
 mySapObject.ApplicationExit(False)
