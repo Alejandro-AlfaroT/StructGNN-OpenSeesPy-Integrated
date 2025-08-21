7465e8da4c03fcc217ab8d0dcf0f10276e42702a
Mpascual7 <mnwpascual@gmail.com>
2025-08-20 21:58:16 -0700

    update

--- MP TESTING/Update Output Features.py
+++ MP TESTING/Update Output Features.py
@@ -15,24 +15,28 @@ print("num_nodes:", num_nodes, "| old y shape:", tuple(data.y.shape))
 # 3) Build your new node-level targets (shape: [num_nodes, new_dim])
 UX = 1
 UY = 1
+
 MZ1 = 0
 MZ2 = 0
 MZ3 = 0
 MZ4 = 0
 MZ5 = 0
 MZ6 = 0
+
 MY1 = 0
 MY2 = 0
 MY3 = 0
 MY4 = 0
 MY5 = 0
 MY6 = 0
+
 VZ1 = 0
 VZ2 = 0
 VZ3 = 0
 VZ4 = 0
 VZ5 = 0
 VZ6 = 0
+
 VY1 = 0
 VY2 = 0
 VY3 = 0
@@ -41,32 +45,11 @@ VY5 = 0
 VY6 = 0
 
 new_y = [
-    UX,
-    UY,
-    MZ1,
-    MZ2,
-    MZ3,
-    MZ4,
-    MZ5,
-    MZ6,
-    MY1,
-    MY2,
-    MY3,
-    MY4,
-    MY5,
-    MY6,
-    VZ1,
-    VZ2,
-    VZ3,
-    VZ4,
-    VZ5,
-    VZ6,
-    VY1,
-    VY2,
-    VY3,
-    VY4,
-    VY5,
-    VY6,
+    UX, UY,
+    MZ1, MZ2,  MZ3, MZ4, MZ5, MZ6,
+    MY1, MY2, MY3, MY4, MY5, MY6,
+    VZ1, VZ2, VZ3, VZ4, VZ5, VZ6,
+    VY1, VY2, VY3, VY4, VY5, VY6
 ]
 
 
